c10acaa759554137a5620b3e245366b4c6e76685
Goraychenok <55753151+Goraychenok@users.noreply.github.com>
2021-03-31 02:22:08 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -39,7 +39,49 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
       },
   });
-  
 
 
+
+
+
+});
+
+$('.popup-btn').on('click',  function(event) {
+  event.preventDefault();
+  $('.popup').fadeIn();
+$('body').addClass('stop');
+$('.header').addClass('none');
+});
+
+$(document).mouseup(function (e){ // событие клика по веб-документу
+  var popup = $("#popup-dialog"); // тут указываем ID элемента
+  var close = $(".popup-close");
+  $('body').addClass('stop');
+  if (!popup.is(e.target)// если клик был не по нашему блоку
+      && popup.has(e.target).length === 0 // и не по его дочерним элементам
+      || close.is(e.target)) { 
+          $('body').addClass('stop');
+          event.preventDefault();
+          $('.popup').fadeOut();
+    $('body').removeClass('stop');
+    $('.header').removeClass('none');
+  }
+});
+
+function slowScroll(id){
+	var offset = 10;
+	$('html,body').animate ({
+		scrollTop: $(id).offset ().top - offset
+	},1000);
+	$('body').removeClass('lock');
+	$('.header__burger,.header__menu').removeClass('active');
+	return false;
+}
+
+jQuery(document).ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+        $('.header').addClass('active');
+    });
 });
